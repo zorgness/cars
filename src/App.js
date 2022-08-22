b2e3158bdc4c03dd7671b5bdc430b26b7c35f9bc
Thomas Fouquert <tfouquert@gmail.com>
2022-08-22 19:19:54 +0200

    state

--- src/App.js
+++ src/App.js
@@ -8,10 +8,35 @@ class App extends React.Component {
     color: 'green'
   }
 
+  changeTitle = () => {
+    this.setState({title: 'New cars'});
+  }
+
+  changeViaParam = (title) => {
+    this.setState({title})
+  }
+
+  changeViaBind(param) {
+    this.setState({
+      title: param
+    })
+  }
+  changeViaInput = (e) => {
+    this.setState({
+      title: e.target.value
+    })
+  }
+
   render() {
   return (
     <div className="App">
         <Mycars title={this.state.title} color={this.state.color}/>
+
+        <button onClick={this.changeTitle}>change title</button>
+        <button onClick={() => this.changeViaParam('Via param')}>change title param</button>
+        <button onClick={this.changeViaBind.bind(this, 'Via bind')}>change title bind</button>
+
+        <input type="text" onChange={this.changeViaInput} value={this.state.title} placeholder="change by input" />
     </div>
 
   );
--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -3,6 +3,7 @@ import Car from './Cars';
 import capitalizeFirst from '../utils/Capitalizefirst';
 import Wrapper from './Wrapper';
 import Myheader from './Myheader';
+import '../index.css';
 
 class Mycars extends Component {
 
@@ -10,19 +11,50 @@ class Mycars extends Component {
     cars: ['mercedes', 'renault', 'peugeot']
   }
 
+  noCopy = () => {
+    alert("you can't copy this text");
+  }
+
+  mouseOver = (e) => {
+    console.log(e.target);
+    if(e.target.classList.contains('styled')) {
+      e.target.classList.remove('styled');
+    } else {
+      e.target.classList.add('styled');
+    }
+  }
+
+  changeSubTitle = (e) => {
+   console.log(e.target);
+   const title = document.querySelector('h3');
+   if(title.innerHTML === 'lol') {
+    title.innerHTML = 'Amet consectetur';
+   } else {
+    title.innerHTML = 'lol';
+   }
+
+
+  }
+
   render() {
 
-    const title = this.props.title
+    const title = this.props.title;
     const colortitle = this.props.color;
+
+
 
 
     return (
       <div>
-        <Wrapper><Myheader myStyle={ colortitle }>{ title }</Myheader></Wrapper>
+        <Wrapper><Myheader myStyle={ colortitle } onMouseOver={ this.mouseOver }>{ title }</Myheader></Wrapper>
+        <h3 onMouseOver={ this.mouseOver }>Amet consectetur</h3>
+        <p onCopy={this.noCopy}>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
         <Car brand={capitalizeFirst(this.state.cars[0])} color="grey" />
         <Car brand={capitalizeFirst(this.state.cars[1])} />
         <Car brand={capitalizeFirst(this.state.cars[2])} color="black"/>
         <Car />
+
+        <button onClick={ this.changeSubTitle }>change subtitle</button>
 
 
       </div>
