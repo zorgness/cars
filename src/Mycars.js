9665a969b6edc11c29c5b5c3f028d834c125b2b5
Thomas Fouquert <tfouquert@gmail.com>
2022-08-22 17:11:32 +0200

    update

--- src/Mycars.js
+++ src/Mycars.js
@@ -5,9 +5,11 @@ class Mycars extends Component {
 
   render() {
 
+    const title = this.props
+
     return (
       <div>
-        <h1>{ this.props.title }</h1>
+        <h1>{ title }</h1>
         <Car brand="Mercedes" color="grey" />
         <Car brand="Renault" />
         <Car brand="Peugeot" color="black"/>
