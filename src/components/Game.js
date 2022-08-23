161e82034e276475ef11db988f9c4795b9af6f66
Thomas Fouquert <tfouquert@gmail.com>
2022-08-23 12:33:28 +0200

    condition

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,15 +3,32 @@ import React, { Component } from 'react'
 class Game extends Component {
 
   state = {
-    player: "mario",
+    player: "Mario",
     winner: true
   }
+
   render() {
-    return (
+    let result = "";
+    // if(this.state.winner) {
+    //   result = <h3>Bravo {this.state.player}</h3>
+
+    // } else {
+    //   result = <h3>Nul {this.state.player}</h3>
+    // }
+
+    this.state.winner ? result = <h3>Bravo {this.state.player}</h3> : result = <h3>Nul {this.state.player}</h3>
+
+    this.state.winner && console.log("win");
+
+    return(
       <div>
         <h1>Game</h1>
+          {result}
       </div>
     )
+
+
+
   }
 }
 
